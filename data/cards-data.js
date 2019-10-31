bf8a20e00fc1932457a30107b095ff1eec961a61
ogonzal87 <ogonzal87@gmail.com>
2019-10-31 06:00:51 -0700

    pushing latest

--- data/cards-data.js
+++ data/cards-data.js
@@ -1,58 +1,64 @@
 const cardsData = [
     {
-        name: "Bulbasaur",
-        damage: 60
+        name: "Venonat",
+        damage: 64
     }, {
         name: "Caterpie",
         damage: 40
     }, {
-        name: "Charmander",
-        damage: 60
-    }, {
-        name: "Clefairy",
-        damage: 50
-    }, {
-        name: "Jigglypuff",
-        damage: 60
-    }, {
-        name: "Mankey",
-        damage: 30
-    }, {
-        name: "Meowth",
-        damage: 60
-    }, {
-        name: "Nidoran - female",
-        damage: 60
-    }, {
-        name: "Nidoran - male",
-        damage: 50
-    }, {
-        name: "Oddish",
-        damage: 40
-    }, {
-        name: "Pidgey",
-        damage: 50
-    }, {
-        name: "Pikachu",
-        damage: 50
-    }, {
-        name: "Poliwag",
-        damage: 50
-    }, {
-        name: "Psyduck",
-        damage: 60
-    }, {
-        name: "Rattata",
-        damage: 30
-    }, {
-        name: "Squirtle",
-        damage: 60
-    }, {
-        name: "Vulpix",
-        damage: 50
+        name: "Abra",
+        damage: 62
     }, {
         name: "Weedle",
         damage: 40
+    }, {
+        name: "Mew",
+        damage: 60
+    }, {
+        name: "Snorlax",
+        damage: 56
+    }, {
+        name: "Zubat",
+        damage: 63
+    }, {
+        name: "Dratini",
+        damage: 69
+    }, {
+        name: "Bellsprout",
+        damage: 55
+    }, {
+        name: "Psyduck",
+        damage: 41
+    }, {
+        name: "Mankey",
+        damage: 70
+    }, {
+        name: "Jigglypuff",
+        damage: 80
+    }, {
+        name: "Meowth",
+        damage: 55
+    }, {
+        name: "Pidgey",
+        damage: 23
+    }, {
+        name: "Eevee",
+        damage: 32
+    }, {
+        name: "Bulbasaur",
+        damage: 65
+    }, {
+        name: "Pikachu",
+        damage: 99
+    }, {
+        name: "Charmander",
+        damage: 40
+    }, {
+        name: "Squirtle",
+        damage: 20
+    }, {
+        name: "Rattata",
+        damage: 80
     }
 ]
 
