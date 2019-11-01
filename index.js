8c61537ee5c0d0be0e3d536037d0088ca54a4f8c
Oscar Gonzalez <ogonzal87@gmail.com>
2019-10-31 17:22:49 -0700

    pushing latest

--- index.js
+++ index.js
@@ -35,11 +35,11 @@ class Game {
         let damageOfUserSelection = getUserSelection[0].damage;
 
         if (damageOfComputerSelection > damageOfUserSelection) {
-            this.computerPoints = ++this.computerPoints;
-            document.getElementById("player1-points").innerHTML = game.userPoints;
+            game.userPoints = ++game.userPoints;
+            document.getElementById("player2-points").innerHTML = game.userPoints;
         } else {
-            this.userPoints = ++this.userPoints;
-            document.getElementById("player2-points").innerHTML = game.computerPoints;
+            game.computerPoints = ++game.computerPoints;
+            document.getElementById("player1-points").innerHTML = game.computerPoints;
         }
 
         this.draws = ++this.draws
@@ -48,11 +48,11 @@ class Game {
             document.getElementById("rounds").innerHTML = game.rounds;
 
             if (this.userPoints > this.computerPoints) {
-                document.getElementById("winner-message").innerHTML = `${humanPlayer.playerName} wins this round!!`;
+                document.getElementById("round-winner-message").innerHTML = `${computerPlayer.playerName} wins this round`;
             } else if (this.computerPoints > this.userPoints) {
-                document.getElementById("winner-message").innerHTML = `${computerPlayer.playerName} wins this round!!`;
+                document.getElementById("round-winner-message").innerHTML = `${humanPlayer.playerName} wins this round`;
             } else {
-                document.getElementById("winner-message").innerHTML = `${computerPlayer.playerName} and ${humanPlayer.playerName} are tied!`;
+                document.getElementById("round-winner-message").innerHTML = `${computerPlayer.playerName} and ${humanPlayer.playerName} are tied`;
             }
         }
     }
@@ -61,6 +61,7 @@ class Game {
         let shuffled = this.cards.sort(() => { return .5 - Math.random() });
         let selected = shuffled.slice(0, 1);
 
+        document.getElementById("damage-number-user").innerHTML = selected[0].damage;
         document.getElementById('pokemon-image-user').src = `./assets/images/${selected[0].name}.svg`;
 
         arrayRemove(this.cards, selected);
@@ -73,6 +74,7 @@ class Game {
         let shuffled = this.cards.sort(() => { return .5 - Math.random() });
         let selected = shuffled.slice(0, 1);
 
+        document.getElementById("damage-number-computer").innerHTML = selected[0].damage;
         document.getElementById('pokemon-image-computer').src = `./assets/images/${selected[0].name}.svg`;
 
         arrayRemove(this.cards, selected);
@@ -87,10 +89,6 @@ const game = new Game(nameOfPlayer, nameOfComputer);
 document.getElementById("player1-name").innerHTML = humanPlayer.playerName;
 document.getElementById("player2-name").innerHTML = computerPlayer.playerName;
 document.getElementById("draw").addEventListener("click", game.draw);
-
-const imagess = document.getElementById("image").innerHTML = cardsData[0].image;
-
-console.log(imagess)
 
 function arrayRemove(arr, value) {
     for (var i = 0; i < arr.length; i++) {
